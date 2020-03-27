cc5c395329859733ce41a3904b64c35ed999b9d0
Gabriel Claveau-Baril <gclaveau@effenti.ca>
2020-03-27 13:26:03 -0400

    changement type de navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {SharedModule} from './components/shared/shared.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {CompetencesComponent} from './pages/competences/competences.component';
+import {ExperienceComponent} from './pages/experience/experience.component';
+import {FormationsComponent} from './pages/formations/formations.component';
+import {MandatsComponent} from './pages/mandats/mandats.component';
+import {PresentationComponent} from './pages/presentation/presentation.component';
+import { LayoutComponent } from './pages/layout.component';
+import { LangageComponent } from './pages/langage/langage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompetencesComponent,
+    ExperienceComponent,
+    FormationsComponent,
+    MandatsComponent,
+    PresentationComponent,
+    LayoutComponent,
+    LangageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
